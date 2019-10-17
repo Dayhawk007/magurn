ec973b841a79a2cc0a352dacdf9cbbe1bb2137bb
Shivam Garg <sgshivamgarg8@gmail.com>
2019-10-18 04:27:41 +0530

    updated main function, readme file and executable file

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-# magurn
-A python application which scrapes data from torrent indexers and finds the best torrent magnet for the specific search term.
+# Magurn
 
-Included an EXE file to run directly for windows.
+## A python application which scrapes data from torrent indexers and finds the best torrent magnet for the specific search term.
+
+* Included an EXE file to run directly for windows.
 
 Requirements-
+```
+pip install requests bs4 pandas
+```
 
-pip install requests
-
-pip install bs4
-
-pip install pandas
-
-If torrent sites are blocked in your region use a VPN or Proxy else the application won't work.
+- If torrent sites are blocked in your region use a VPN or Proxy else the application won't work.
--- TorrentSearch.py
+++ TorrentSearch.py
@@ -3,25 +3,17 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
-searchterm=input("Enter the name of torrent you want to search\n")
-tor_seed={}
-names=[]
-urls=[]
-seeds=[]
-magnets=[]
-
 
 def copyToClipBoard(text):
     command = 'echo ' + text.strip() + '| clip'
     os.system(command)
-
 
 def _1337x(search):
     url_f=[]
     search_l=search.split()
 
     search_name="+".join(search_l)
-    req_url="https://www.1337x.to/search/"+str(search_name)+"/1/"
+    req_url="https://1337x.to/search/"+str(search_name)+"/1/"
     res=requests.get(req_url,headers={
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
     })
@@ -30,7 +22,7 @@ def _1337x(search):
     for link in soup.find_all('a'):
         if search.lower() in link.text.lower():
             names.append(link.text.strip())
-            url_f.append("https://www.1337x.to"+link.get('href'))
+            url_f.append("https://1337x.to"+link.get('href'))
             link_con += 1
         if link_con==2:
             break
@@ -82,18 +74,25 @@ def idope(search):
     for x in url_f:
         urls.append(x)
 
-print("Scraping from idope....")
-idope(searchterm)
-print("Scraping from 1337x....")
-_1337x(searchterm)
-tor_seed["Names"]=names
-tor_seed["Links"]=urls
-tor_seed["Seeders"]=seeds
-tor_seed["Magnets"]=magnets
-df=pd.DataFrame(tor_seed)
-df.sort_values('Seeders')
-print("Name:\n"+df["Names"][0])
-print("Magnet Link:\n"+df["Magnets"][0])
-copyToClipBoard(df["Magnets"][0])
-print("Magnet Copied to ClipBoard")
-input("Press Any Key to Close")
+while(1):
+    tor_seed={}
+    names=[]
+    urls=[]
+    seeds=[]
+    magnets=[]
+    searchterm=input("Enter the name of torrent you want to search\n")
+    print("Scraping from idope....")
+    idope(searchterm)
+    print("Scraping from 1337x....")
+    _1337x(searchterm)
+    tor_seed["Names"]=names
+    tor_seed["Links"]=urls
+    tor_seed["Seeders"]=seeds
+    tor_seed["Magnets"]=magnets
+    df=pd.DataFrame(tor_seed)
+    df.sort_values('Seeders')
+    print("Name:\n"+df["Names"][0])
+    print("Magnet Link:\n"+df["Magnets"][0])
+    copyToClipBoard(df["Magnets"][0])
+    print("Magnet Copied to ClipBoard")
+    print("Press Ctrl+C to Close")
