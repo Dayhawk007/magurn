7167d237c951f2a5499702eb16738b8abda8e73a
Shivam Garg <sgshivamgarg8@gmail.com>
2019-12-26 00:07:47 +0530

    getting magnet link of only the result torrent so app is insanely fast

--- magurn/TorrentSearch.py
+++ magurn/TorrentSearch.py
@@ -35,12 +35,11 @@ def check(search, link):
 
 
 def _1337x(search):
-    url_f = []
+    # url_f = []
     search_l = search.split()
     search_name = "+".join(search_l)
 
     # base_url = "https://1337x.to"
-    # base_url = "https://1337x.wtf"  # PROXIED URL
     base_url = "https://x1337.info"  # PROXIED URL
     req_url = base_url + "/sort-search/" + str(search_name) + "/time/desc/1/"
     try:
@@ -68,9 +67,12 @@ def _1337x(search):
         if seed < min_seeds:  # Seeds should be greater than/equal to min_seeds
             continue
 
+        origin.append('1337x')
+
         seeds.append(seed)
 
-        url_f.append(base_url + link.get("href"))
+        links.append(base_url + link.get("href"))
+
         names.append(link.text.strip())
 
         uploaded.append(row.find('td', attrs={'class': 'coll-date'}).text)
@@ -82,23 +84,12 @@ def _1337x(search):
         if data_count == 2:
             break
 
-    for url in url_f:
-        url_res = requests.get(url, headers=headers)
-        urlsoup = BeautifulSoup(url_res.content, features="html.parser")
-
-        for magnet in urlsoup.find_all("a"):
-            try:
-                if "magnet" in magnet.text.split()[0].lower():
-                    magnets.append(magnet.get("href"))
-            except:
-                pass
-
     if not data_count:
         print(Fore.RED + "Nothing Found on 1337x")
 
 
 def idope(search):
-    url_f = []
+    # url_f = []
     # base_url = "https://idope.se"
     base_url = "https://gv6zipaqcoaau4qe.onio.icu"  # PROXIED URL
     req_url = base_url + "/torrent-list/" + str(search) + "/?&o=-3"
@@ -122,9 +113,13 @@ def idope(search):
         if int(seed.text) < min_seeds:  # Seeds should be greater than/equal to min_seeds
             continue
 
+        origin.append('idope')
+
         uploaded.append(
             str(div.find('div', attrs={'class': 'resultdivbottontime'}).text) + " Ago")
-        url_f.append(base_url + link.get("href"))
+
+        links.append(base_url + link.get("href"))
+
         names.append(link.text.strip())
 
         seeds.append(int(seed.text))
@@ -136,18 +131,12 @@ def idope(search):
         if data_count == 2:
             break
 
-    for url in url_f:
-        url_res = requests.get(url, headers=headers)
-        urlsoup = BeautifulSoup(url_res.content, features="html.parser")
-        for magnet in urlsoup.find_all(id="mangetinfo"):
-            magnets.append(magnet.text.strip())
-
     if not data_count:
         print(Fore.RED + "Nothing Found on idope")
 
 
 def piratebay(search):
-    url_f = []
+    # url_f = []
     # base_url = "https://thepiratebay.org"
     base_url = "https://247prox.link"  # PROXIED URL
     # base_url = piratebay_proxy_base_url
@@ -167,6 +156,8 @@ def piratebay(search):
             c = True
             continue
 
+        # print(tr)
+
         link = tr.div.a
 
         if not check(search, link):
@@ -177,9 +168,11 @@ def piratebay(search):
         if int(td[-2].text) < min_seeds:  # Seeds should be greater than/equal to min_seeds
             continue
 
+        origin.append('piratebay')
+
         names.append(link.text.strip())
 
-        url_f.append(str(base_url + link.get("href")))
+        links.append(str(base_url + link.get("href")))
 
         size_data = tr.font.text.split(",")[1].strip()
         size_value = size_data.split()[1]
@@ -189,29 +182,40 @@ def piratebay(search):
 
         seeds.append(int(td[-2].text))
 
+        uploaded.append(tr.font.text.split(",")[0].strip().split(' ')[1])
+
         data_count += 1
         if data_count == 2:
             break
-
-    for url in url_f:
-        url_res = requests.get(url, headers=headers)
-        urlsoup = BeautifulSoup(url_res.content, features="html.parser")
-
-        uploaded.append(urlsoup.find_all('dd')[-6].text.split()[0])
-
-        div = urlsoup.find("div", attrs={"class": "download"})
-
-        magnets.append(div.a.get("href"))
 
     if not data_count:
         print(Fore.RED + "Nothing Found on Piratebay")
 
 
+def getMagnet(origin, link):
+    url_res = requests.get(link, headers=headers)
+    urlsoup = BeautifulSoup(url_res.content, features="html.parser")
+    # print(urlsoup.prettify())
+    if origin=='1337x':
+        return urlsoup.find('a', attrs={'class': 'leab53b8068c0a628e11a907243961498a431c3f3 lb8818a071d284a7b8b1fc267080df6950674c626 l04e159a1db97662974f8701afa276351269fd59d'}).get('href')
+    
+    elif origin == 'idope':
+        # print(urlsoup.prettify())
+        magnet = urlsoup.find('a', {'id':"mangetinfo"}).get('href')
+        return magnet
+
+    elif origin == 'piratebay':
+        div = urlsoup.find("div", attrs={"class": "download"})
+        return (div.a.get("href"))
+
+
 while 1:
     tor_seed = {}
+    origin = []
+    links=[]
     names = []
     seeds = []
-    magnets = []
+    # magnets = []
     sizes = []
     uploaded = []
 
@@ -255,16 +259,20 @@ while 1:
         score.append(seeds[i]/size_in_mb[i])
 
     tor_seed["Names"] = names
+    tor_seed["Origin"] = origin
+    tor_seed["Links"] = links
     tor_seed["Sizes"] = sizes
     tor_seed["Seeders"] = seeds
     tor_seed["Uploaded"] = uploaded
     tor_seed["SizesMB"] = size_in_mb
-    tor_seed["Magnets"] = magnets
+    # tor_seed["Magnets"] = magnets
     tor_seed["Score"] = score
 
     # print(tor_seed)
 
     maxIndex = score.index(max(score))
+
+    magnet = getMagnet(tor_seed['Origin'][maxIndex], tor_seed['Links'][maxIndex])
 
     print(Fore.MAGENTA + "Name: " + Style.BRIGHT + tor_seed["Names"][maxIndex])
     print(Fore.MAGENTA + "Size: " + Style.BRIGHT + tor_seed["Sizes"][maxIndex])
@@ -272,10 +280,10 @@ while 1:
           str(tor_seed["Seeders"][maxIndex]))
     print(Fore.MAGENTA + "Uploaded: " + Style.BRIGHT +
           tor_seed["Uploaded"][maxIndex])
-    print(Fore.BLUE + "Magnet Link:\n" +
-          Style.BRIGHT + tor_seed["Magnets"][maxIndex])
+    print(Fore.BLUE + 'Torrent Link: ' + Style.BRIGHT + tor_seed['Links'][maxIndex])
+    print(Fore.BLUE + "Magnet Link:\n" + Style.BRIGHT + magnet)
     try:
-        copyToClipBoard(tor_seed["Magnets"][maxIndex])
+        copyToClipBoard(magnet)
         print(Fore.GREEN + "Magnet Copied to ClipBoard")
     except:
         pass
